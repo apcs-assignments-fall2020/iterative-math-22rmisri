52b2bc641f9ae484df43bdc1a79188308b528910
22rmisri <22rmisri@athenian.org>
2020-09-13 17:36:31 +0000

    wrote code for Iterative Math Methods

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,36 +1,52 @@
 import java.util.Scanner;
-
 public class MyMain {
     
     // Calculates the square root iteratively, using the Babylonian method
     public static double babylonian(double x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        double n = 10;
+        double m;
+        double y;
+        do { 
+            m = (n + (x/n))/2;
+            System.out.println(m);
+            y = m - n;
+            y = Math.abs(y);
+            n = m;
+        }
+        while ( y >  0.00001);
+        return m;
     }
-
     // Calculates the factorial of a number
     public static double factorial(int x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        double y = 1;
+        for (int i = x; i > 0; i--) {
+            y = y * i;
+        }
+        return y;
     }
 
     // Calculates the value for the math constant e iteratively
     // goes until it calculates an answer less than 0.00000000001 
     // different from the value Math.E
     public static double calculateE() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
-    }
-    
-    
+        int z = 0;
+        double y = 1/factorial(z);
+        double j = Math.E;
+        double difference;
+        do {
+            difference = j - y;
+            z = z + 1;
+            y = y + (1/factorial(z));
+        }
+        while (difference > 0.00001);
+        return y;
+}
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        // YOUR CODE HERE
-
-
-        
-
+        System.out.println("Enter a positive number!");
+        Double num = Double.parseDouble(scan.next());
+        System.out.println("The square root of " + num + " is " + babylonian(num));
+        System.out.println("The value of e is roughly: " + calculateE());
         scan.close();
     }
 }
